45c800423ddcd03ab0c567e76566ae60130e559a
Natoniewski <anatonie@dca9047db951.ant.amazon.com>
2017-12-11 11:32:29 -0800

    auto build pipeline test

--- src/panels/home.tsx
+++ src/panels/home.tsx
@@ -13,9 +13,8 @@ export default class Home extends React.Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>Welcome to Twisty's Blog 2.0</h2>
                 </div>
-                <p className="App-intro">
-                    Major work in progress, just barely got started so take a chill pill
-                </p>
+                <p className="App-intro">Major work in progress, just barely got started so take a chill pill</p>
+                <p>This is a test for auto build</p>
             </Layout>
         );
     }
